456731490ec9ba80a3ce87efb6a0fe8c88087322
Linn Berntsson <linn.berntsson@ess.eu>
2023-03-30 10:37:14 +0200

    Set correct directory for MIB files

--- test/cmds/pv_test.cmd
+++ test/cmds/pv_test.cmd
@@ -7,7 +7,7 @@ epicsEnvSet(USER, "simdata")
 
 epicsEnvSet(TOP, "$(E3_CMD_TOP)/..")
 
-epicsEnvSet("MIBDIRS", "+$(TOP)/mibs")
+epicsEnvSet("MIBDIRS", "+$(TOP)/../mibs")
 
 devSnmpSetSnmpVersion("$(HOST)","SNMP_VERSION_2c")
 
